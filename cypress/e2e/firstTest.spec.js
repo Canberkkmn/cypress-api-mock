de70cf912ef4e1e83c6929b7b691cbece2f2689c
Canberkkmn <canberkkaman@gmail.com>
2024-10-14 00:41:47 +0300

    test: add mock data to api testing

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,5 +1,6 @@
 describe('Test with backend', () => {
   beforeEach('login to app', () => {
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/tags', { fixture: 'tags.json' })
     cy.loginToApp()
   })
 
@@ -7,17 +8,40 @@ describe('Test with backend', () => {
     cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/').as('postArticles')
 
     cy.contains('New Article').click()
-    cy.get('[formcontrolname="title"]').type('This is the title ' + 'Math.random()')
+    cy.get('[formcontrolname="title"]').type('This is the title ' + Math.random())
     cy.get('[formcontrolname="description"]').type('This is the description')
     cy.get('[formcontrolname="body"]').type('This is the body')
     cy.contains('Publish Article').click()
 
     cy.wait('@postArticles')
     cy.get('@postArticles').then(xhr => {
-      console.log(xhr)
       expect(xhr.response.statusCode).to.equal(201)
       expect(xhr.request.body.article.body).to.equal('This is the body')
       expect(xhr.response.body.article.description).to.equal('This is the description')
     })
+  })
+
+  it('verify popular tags are displayed', () => {
+    cy.get('.tag-list').should('contain', 'cypress').and('contain', 'automation').and('contain', 'testing')
+  })
+
+  it('verify global feed likes count', () => {
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles/feed*', { "articles": [], "articlesCount": 0 })
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles*', { fixture: "articles.json" })
+
+    cy.contains('Global Feed').click()
+    cy.get('app-article-list button').then(heartList => {
+      expect(heartList[0]).to.contain('1')
+      expect(heartList[1]).to.contain('5')
+    })
+
+    cy.fixture('articles').then(file => {
+      const articleLink = file.articles[1].slug
+      file.articles[1].favoritesCount = 6
+      cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/' + articleLink + '/favorite', file)
+    })
+
+    cy.get('app-article-list button').eq(1).click().should('contain', '6')
+
   })
 })
