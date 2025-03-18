e141f5993006bd5c4cbe4d6167a4e6d83e167f87
Canberkkmn <canberkkaman@gmail.com>
2025-03-18 20:16:14 +0300

    Fix mochawesome report generation in CI/CD

--- cypress.config.js
+++ cypress.config.js
@@ -5,21 +5,26 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   video: false,
   env: {
-    email: 'can@123.com',
-    password: '123',
-    apiUrl: 'https://conduit-api.bondaracademy.com'
+    email: "can@123.com",
+    password: "123",
+    apiUrl: "https://conduit-api.bondaracademy.com",
   },
   retries: {
     runMode: 2,
-    openMode: 0
+    openMode: 0,
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      require('cypress-mochawesome-reporter')
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-    baseUrl: 'https://conduit.bondaracademy.com/',
-    specPattern: 'cypress/e2e/**.spec.{js,jsx,ts,tsx}'
+    baseUrl: "https://conduit.bondaracademy.com/",
+    specPattern: "cypress/e2e/**.spec.{js,jsx,ts,tsx}",
   },
-  reporter: 'cypress-mochawesome-reporter'
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "mochawesome-report",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
 });
